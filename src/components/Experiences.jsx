36f2351267621e52adeee9b8644b0eae72548889
Ryan <ryaninuyasha58@gmail.com>
2024-06-13 21:35:43 -0400

    experiences

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import ExperienceItem from "./ExperienceItem";
+import ExperienceData from "../data/ExperienceData";
+import Title from "./Title";
 
 function Experiences() {
+  const firstHalf = ExperienceData.slice(
+    0,
+    Math.ceil(ExperienceData.length / 2)
+  );
+  const secondHalf = ExperienceData.slice(Math.ceil(ExperienceData.length / 2));
   return (
-    <div>
-      EXPERIENCES
+    <div className="flex flex-col mb-10 mx-auto mt-[100px] p-8">
+      <Title>Experiences</Title>
+      <div className="flex flex-col md:flex-row justify-center ">
+        <div className="w-full md:w-7/12 mt-12">
+          <ExperienceItem json1={firstHalf} json2={secondHalf} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Experiences
+export default Experiences;
