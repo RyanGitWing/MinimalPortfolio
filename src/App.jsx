27650de07a8fc7a8be032c162c787f1e6d9172a1
Ryan <ryaninuyasha58@gmail.com>
2024-06-13 20:48:53 -0400

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-// import About from "./components/About";
-// import Projects from "./components/Projects";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
-// import Experiences from "./components/Experiences";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Experiences from "./components/Experiences";
+import Skills from "./components/Skills";
 // import Navbar from "./components/Navbar";
-// import Skills from "./components/Skills";
+// import Footer from "./components/Footer";
 // import { ThemeProvider } from "./components/ThemeContext";
 
 function App() {
   return (
     <div>
-      asdasd
+      {/* <ThemeProvider> */}
+      {/* <Navbar /> */}
+      <div id="about">
+        <About />
+      </div>
+      <div id="skills">
+        <Skills />
+      </div>
+      <div id="experiences">
+        <Experiences />
+      </div>
+      <div id="projects">
+        <Projects />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+      <Footer />
+      {/* </ThemeProvider> */}
     </div>
   );
 }
