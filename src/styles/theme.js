7a26158bc10b609fb662b0888c8401f034d1bd63
Ryan <ryaninuyasha58@gmail.com>
2024-06-16 16:10:39 -0400

    cleaned theme code

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,43 @@
 // src/themes.js
 import { createTheme } from '@mui/material/styles';
 
+const commonSettings = {
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+  },
+  breakpoints: {
+    values: {
+      xs: 0,     // default
+      sm: 640,   // TailwindCSS sm
+      md: 768,   // TailwindCSS md
+      lg: 1024,  // TailwindCSS lg
+      xl: 1280,  // TailwindCSS xl
+      xxl: 1536, // TailwindCSS 2xl
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        '@global': {
+          '@font-face': {
+            fontFamily: 'Montserrat',
+            fontStyle: 'normal',
+            fontWeight: '100 900', // Include all font weights
+            fontDisplay: 'swap', // Use swap to ensure text renders with fallback fonts while Montserrat is loading
+            src: `
+              url(https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_aZA3gnD_g.woff2) format('woff2')
+            `,
+          },
+        },
+      },
+    },
+  },
+};
+
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
@@ -17,38 +54,7 @@ export const lightTheme = createTheme({
       background: '#333333'
     },
   },
-  typography: {
-    fontFamily: [
-      'Montserrat',
-      'Arial',
-      'sans-serif',
-    ].join(','),
-  },
-  breakpoints: {
-    values: {
-      xs: 0,     // default
-      sm: 640,   // TailwindCSS sm
-      md: 768,   // TailwindCSS md
-      lg: 1024,  // TailwindCSS lg
-      xl: 1280,  // TailwindCSS xl
-      xxl: 1536, // TailwindCSS 2xl
-    },
-  },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        '@font-face': {
-          fontFamily: 'Montserrat',
-          fontStyle: 'normal',
-          fontWeight: '100 900', // Include all font weights
-          fontDisplay: 'swap', // Use swap to ensure text renders with fallback fonts while Montserrat is loading
-          src: `
-            url(https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_aZA3gnD_g.woff2) format('woff2')
-          `,
-        },
-      },
-    },
-  },
+  ...commonSettings,
 });
 
 export const darkTheme = createTheme({
@@ -65,39 +71,7 @@ export const darkTheme = createTheme({
     accent: {
       main: '#333333',
       background: '#00ffa6'
-
     },
   },
-  typography: {
-    fontFamily: [
-      'Montserrat',
-      'Arial',
-      'sans-serif',
-    ].join(','),
-  },
-  breakpoints: {
-    values: {
-      xs: 0,     // default
-      sm: 640,   // TailwindCSS sm
-      md: 768,   // TailwindCSS md
-      lg: 1024,  // TailwindCSS lg
-      xl: 1280,  // TailwindCSS xl
-      xxl: 1536, // TailwindCSS 2xl
-    },
-  },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        '@font-face': {
-          fontFamily: 'Montserrat',
-          fontStyle: 'normal',
-          fontWeight: '100 900', // Include all font weights
-          fontDisplay: 'swap', // Use swap to ensure text renders with fallback fonts while Montserrat is loading
-          src: `
-            url(https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_aZA3gnD_g.woff2) format('woff2')
-          `,
-        },
-      },
-    },
-  },
+  ...commonSettings,
 });
